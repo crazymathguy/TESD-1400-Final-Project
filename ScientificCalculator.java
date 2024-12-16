a94f3ffb22f461046fb39fb98580aa8312daaea4
crazymathguy <briggs.sea0642@stu.stech.edu>
2024-12-16 09:02:50 -0700

    Add conversion between m and mi

--- ScientificCalculator.java
+++ ScientificCalculator.java
@@ -1,12 +1,28 @@
 import java.util.*;
+import java.awt.print.*;
 
 public class ScientificCalculator {
 	public static void main(String[] args) {
 		// 1 mile = 1609 m
 		String[] input = getUserInput("Enter a value and a unit to convert it to\n(separated by a space: ex. 4.5mi m)");
-		Value value = getValue(input[0]);
-		value.printValue();
-		System.out.print(" = " + input[1]);
+		if (input.length == 2) {
+			Value value = Value.getValue(input[0]);
+			if (value == null) {
+				System.out.print("Invalid known value");
+			} else {
+				Value unitDef = value.getUnitDefinition();
+				Value siValue = new Value(value.value * unitDef.value, unitDef.units);
+				Value unitToConvert = Value.getUnitDefinition(input[1]);
+				if (unitToConvert == null) {
+					System.out.print("Invalid unknown value");
+				} else {
+					Value convertedValue = new Value(siValue.value / unitToConvert.value, unitToConvert.units);
+					printEquation(value, convertedValue);
+				}
+			}
+		} else {
+			System.out.print("Invalid equation");
+		}
 		
 		/* for (int i = 0; i < input.length; i++) {
 			Value value = getValue(input[i]);
@@ -21,19 +37,9 @@ public class ScientificCalculator {
 		return line.split(" ");
 	}
 	
-	static Value getValue(String stringToParse) {
-		String numericalValue = "";
-		String unitsValue = "";
-		for (int i = 0; i < stringToParse.length(); i++) {
-			char current = stringToParse.charAt(i);
-			if (Character.isLetter(current)) {
-				unitsValue += current;
-			}
-			else if (Character.isDigit(current) || current == '.') {
-				numericalValue += current;
-			}
-		}
-		Value returnValue = new Value(Double.parseDouble(numericalValue), unitsValue);
-		return returnValue;
+	static void printEquation(Value known, Value unknown) {
+		known.printValue();
+		System.out.print(" = ");
+		unknown.printValue();
 	}
 }
--- Value.java
+++ Value.java
@@ -7,7 +7,58 @@ public class Value {
 		this.units = units;
 	}
 	
+	static Value getValue(String stringToParse) {
+		String numericalValue = "";
+		String unitsValue = "";
+		for (int i = 0; i < stringToParse.length(); i++) {
+			char current = stringToParse.charAt(i);
+			if (Character.isLetter(current)) {
+				unitsValue += current;
+			}
+			else if (Character.isDigit(current) || current == '.') {
+				numericalValue += current;
+			}
+		}
+		Value returnValue = new Value(Double.parseDouble(numericalValue), unitsValue);
+		if (returnValue.isValid()) {
+			return returnValue;
+		} else {
+			return null;
+		}
+	}
+	
 	public void printValue() {
 		System.out.print(this.value + this.units);
+	}
+	
+	public boolean isValid() {
+		return isValidUnit(this.units);
+	}
+	
+	public static boolean isValidUnit(String tryUnit) {
+		switch (tryUnit) {
+			case "m":
+			case "mi":
+				return true;
+			default:
+				return false;
+		}
+	}
+	
+	public Value getUnitDefinition() {
+		return getUnitDefinition(this.units);
+	}
+	
+	public static Value getUnitDefinition(String units) {
+		if (!isValidUnit(units))
+			return null;
+		switch (units) {
+			case "m":
+				return new Value(1f, "m");
+			case "mi":
+				return new Value(1609f, "m");
+			default:
+				return null;
+		}
 	}
 }
